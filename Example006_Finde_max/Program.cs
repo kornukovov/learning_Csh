5c4d006a68a69dfde746199683f886bc355fd057
kornukovov <kornukovov@yandex.ru>
2023-06-11 17:43:04 +0300

    add_Example006_Finde_max

---
+++ Example006_Finde_max/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 3;
+int b = 4;
+int c = 1;
+int d = 2;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+
+Console.WriteLine($"max = {max}");
