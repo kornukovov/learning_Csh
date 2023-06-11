72ab2cb8bf166ae81b880a88e4738972b7e573a4
kornukovov <kornukovov@yandex.ru>
2023-06-11 17:29:25 +0300

    add_Example005_Condition_if_else

---
+++ Example005_Condition_if_else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter your name");
+string name = Console.ReadLine();
+if (name.ToLower() == "alex")
+{
+   Console.WriteLine("Salute Alex!!!");
+}
+else
+{
+   Console.WriteLine($"Hello {name}");
+}
